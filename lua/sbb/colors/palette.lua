4570b00b75d19af201498fb1d35827692f4edc5b
Dmitry Fedorov <sbb775_photo@yahoo.com>
2026-05-29 21:41:20 +0500

    fix error

--- lua/sbb/colors/palette.lua
+++ lua/sbb/colors/palette.lua
@@ -1,7 +1,45 @@
 local util = require("sbb.util")
 
 local palette = {}
-local colors = {
+-- local colors = {
+--   orange = "#D6A375",
+--   orange_bright = "#F57A0D",
+--
+--   blue = "#9ACDE3",
+--   blue_bright = "#0DAEF5",
+--
+--   green = "#ABE3D0",
+--   green_bright = "#47FFC1",
+--
+--   red = "#E39A96",
+--   red_bright = "#D6746F",
+--
+--   yellow = "#F5F3C2",
+--   yellow_bright = "#D6D375",
+--
+--   purple = "#DAC2F5",
+--   purple_bright = "#A375D6",
+--
+--   cyan = "#9AE3E1",
+--   cyan_bright = "#0DF5EE",
+--
+--   neutral = "#787B79",
+--
+--   gray = "#222728",
+--   gray1 = "#3B3F3F",
+--   gray2 = "#535756",
+--   gray3 = "#6C6F6D",
+-- }
+--
+-- palette.dark = util.deep_merge(colors, {
+--   bg = "#0A0F11",
+--   fg = "#E6E6E1",
+-- })
+
+palette.dark = {
+  bg = "#0A0F11",
+  fg = "#E6E6E1",
+
   orange = "#D6A375",
   orange_bright = "#F57A0D",
 
@@ -30,10 +68,5 @@ local colors = {
   gray2 = "#535756",
   gray3 = "#6C6F6D",
 }
-
-palette.dark = util.deep_merge(colors, {
-  bg = "#0A0F11",
-  fg = "#E6E6E1",
-})
 
 return palette
