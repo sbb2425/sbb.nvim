22518885d78acfb906a7c72c4aac88a672dd9aa5
Dmitry Fedorov <sbb775_photo@yahoo.com>
2026-07-14 23:19:15 +0500

    update systax color

--- lua/sbb/colors/init.lua
+++ lua/sbb/colors/init.lua
@@ -129,11 +129,11 @@ function M.setup(opts)
     identifiers = colors.ui.fg.default,
     functions = colors.accent.orange.default,
 
-    keywords = colors.ui.fg.dark,
+    keywords = colors.accent.neutral.default,
     operators = colors.accent.gray.soft,
 
     types = colors.accent.red.default,
-    members = colors.accent.neutral.default,
+    members = colors.ui.fg.dark,
     links = colors.accent.blue.default,
   }
   colors.diag = {
