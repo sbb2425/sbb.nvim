d6b2e8e0957dcbfb5547557f54b625f7e897d4e4
Dmitry Fedorov <sbb775_photo@yahoo.com>
2026-07-16 11:14:38 +0500

    update blink themes color

---
+++ lua/sbb/groups/plugins/blink.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+---@type sbb.HighlightsFn
+function M.get(opts, c)
+return {
+
+-- окно меню
+BlinkCmpMenu = { fg = c.accent.gray.default, bg = c.ui.bg.soft},
+
+  -- граница меню
+BlinkCmpMenuBorder = {fg = c.accent.cyan.default, bg = c.ui.bg.soft},
+
+  -- выбранный элемент
+BlinkCmpMenuSelection = { fg = c.ui.fg.default, bg = c.ui.bg.light},
+
+  -- текст элемента
+-- BlinkCmpLabel
+  
+  -- иконка типа
+-- BlinkCmpKind
+
+  -- для конкретных типов (например, BlinkCmpKindFunction)
+-- BlinkCmpKind<kind> - 
+    }
+end
+
+return M
